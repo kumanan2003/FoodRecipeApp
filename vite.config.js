fb7a909f4f36500fb3e423ab10e01de51c0780d8
kumanan <kumanan637@gmail.com>
2025-08-09 17:50:26 +0530

    adding deploy for built

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "https://kumanan2003.github.io/FoodRecipeApp/",
+  plugins: [react()]
 })
